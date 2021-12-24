5db23030afc73eb6c0d35a7dbbb099adde4c4357
Jovan Babic <jbabic999@gmail.com>
2021-12-24 18:09:25 +0100

    Frontend init

---
+++ fe-user-management/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   canActivate: [AuthGuard]
+  // },
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
